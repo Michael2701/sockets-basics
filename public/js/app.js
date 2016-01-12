a922e591c8dad4b5f460da9cb2ae2f1695be791b
Michael Silianov <michaelsilianov@gmail.com>
2016-01-12 11:50:15 +0200

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,23 @@
 var socket = io();
-
+var name = getQueryVariable('name') || 'anonymous';
+var room = getQueryVariable('room');
 
 socket.on('connect', function(){
     console.log('Connected to socket.io server!')
 });
 
+//jQuery('.messages').append('<br>'+ name+' joined '+room+' room');
+
 socket.on('message', function(message){
     console.log('New message:');
     console.log(message.text);
+    
     var timeStamp = moment.utc(message.timestamp);
-    jQuery('.messages').append('<p><strong>'+ timeStamp.local().format('h:mma ')+'</strong>' + message.text + '</p>');
+    var $message = jQuery('.messages');
+   
+    $message.append('<p><strong>' + message.name + '  ' + timeStamp.local().format('h:mma ')+'</strong>' + '</p>');
+    $message.append('<p>' + message.text + '</p>');
+       
 });
 
 //handles submitting of new message
@@ -21,7 +29,8 @@ $form.on('submit',function(event){
    var $message = $form.find("input[name='message']");
   
    socket.emit('message',{
-       text: $message.val() 
+       text: $message.val() ,
+       name: name
    });
    $message.val('');
    
