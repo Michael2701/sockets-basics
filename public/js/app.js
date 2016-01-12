8fc94f9127419413225e595489a71bc29e7d638b
Michael Silianov <michaelsilianov@gmail.com>
2016-01-12 10:45:24 +0200

    show time stamp on UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 var socket = io();
 
+
 socket.on('connect', function(){
     console.log('Connected to socket.io server!')
 });
@@ -7,18 +8,20 @@ socket.on('connect', function(){
 socket.on('message', function(message){
     console.log('New message:');
     console.log(message.text);
-    
-    jQuery('.messages').append('<p>'+ message.text +'</p>');
+    var timeStamp = moment.utc(message.timestamp);
+    jQuery('.messages').append('<p><strong>'+ timeStamp.local().format('h:mma ')+'</strong>' + message.text + '</p>');
 });
 
 //handles submitting of new message
 var $form = jQuery('#message-form');
 
+
 $form.on('submit',function(event){
    event.preventDefault();
    var $message = $form.find("input[name='message']");
+  
    socket.emit('message',{
-       text: $message.val()
+       text: $message.val() 
    });
    $message.val('');
    
